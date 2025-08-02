97bf0fe9c7711b44e66be0000f3ae6c0d49a89c3
ahmadrasheed <ahmadrasheedaaa@gmail.com>
2025-08-02 19:09:49 +0500

    Add Prettier configuration and implement Cloudinary upload utility
    - Created .prettierignore to exclude specific files and directories from formatting.
    - Added .prettierrc for Prettier configuration settings.
    - Implemented Cloudinary upload functionality in cloudinary.js, including error handling and file deletion after upload.
    - Defined ApiError class for standardized error handling.

---
+++ BackEnd/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINRAY_CLOUD_NAME,
+  api_key: process.env.CLOUDINRAY_API_KEY,
+  api_secret: process.env.CLOUDINRAY_API_SECRET,
+});
+
+const uploadToCloudinary = async function (localFilePath) {
+  if (localFilePath?.trim() == "") {
+    return null;
+  }
+
+  try {
+    const uploadResult = await cloudinary.uploader.upload(
+      localFilePath,
+      {
+        resource_type: "auto",
+      }
+    );
+    //delete file from db after saving
+    fs.unlinkSync(localFilePath);
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
