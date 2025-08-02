43fc52244b4eba85ef206ccdf58038a9061907e6
ahmadrasheed <ahmadrasheedaaa@gmail.com>
2025-08-02 19:13:13 +0500

    Implement ApiResponse class for standardized response handling

---
+++ BackEnd/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+//in order to send a standarized response
+class ApiResponse {
+  constructor(statusCode, data, message = "Success") {
+    this.data = data;
+    this.statusCode = statusCode;
+    //data will be an object
+    this.data = data;
+    this.success = statusCode < 400;
+  }
+}
