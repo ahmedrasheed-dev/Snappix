5beb09023a9c43023873343accd0f7e6c551a2a0
ahmadrasheed <ahmadrasheedaaa@gmail.com>
2025-08-02 19:23:06 +0500

    Add asyncHandler utility for error handling in controllers

---
+++ BackEnd/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+//higher order function that wraps arround your controller
+//to avoid writing try catch again and again
+const asyncHandler = (fn) => {
+  return (error, req, res, next) => {
+    Promise.resolve(fn(error, req, res, next)).catch(
+      (err) => next(err)
+    );
+  };
+};
